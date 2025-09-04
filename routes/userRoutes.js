0a9d3ccffd962cf06fae81b8713504d299292e69
hemandh <hemandhtsujil@gmail.com>
2025-09-04 11:00:22 +0530

    fixing a bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -70,7 +70,7 @@ router.get('/support', userController.getSupportPage);
 router.get('/contact', userController.getContactPage);
 
 //payment
-router.get('/retry-payment', ...protect, userController.retryPayment);
+router.post('/retry-payment', ...protect, userController.retryPayment);
 
 //whishlist
 router.post('/add-to-cart-from-wishlist/:productId', ...protect, userController.addToCartFromWishlist);
