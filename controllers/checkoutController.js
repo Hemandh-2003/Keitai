98c935358e87a1f7182fd77ca994e44ebd340d97
hemandh <hemandhtsujil@gmail.com>
2025-10-23 16:20:48 +0530

    fixing the pendings

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -7,7 +7,6 @@ const {HTTP_STATUS}= require('../SM/status');
 const { MESSAGE } = require('../SM/messages');
 const Razorpay = require('razorpay'); 
 require('dotenv').config();
-const Razorpay = require('razorpay'); 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
